580de66ed7bd7769a3b05e5580035403267c128d
Nitish kumar <nitish.bpns@gmail.com>
2024-02-29 11:20:08 +0530

    fix hover on amneties

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -47,7 +47,14 @@ const Products = () => {
           <div className={styles.cardItem}>
             <Image src={ic2} width={100} className={styles.icImg} alt="img" />
             <div className={styles.desc}>Restaurant</div>
-
+            <div className={styles.detailHover}>
+              Savor exquisite culinary delights at our resort's restaurant,
+              where every dish is a culinary masterpiece crafted from the
+              freshest locally sourced ingredients. Whether indulging in
+              traditional flavors or exploring innovative gastronomic creations,
+              our restaurant promises a dining experience that tantalizes the
+              senses and leaves a lasting impression of culinary excellence.
+            </div>
           </div>
         </div>
         <div className={styles.amCard}>
@@ -61,7 +68,14 @@ const Products = () => {
           <div className={styles.cardItem}>
             <Image src={ic3} width={100} className={styles.icImg} alt="img" />
             <div className={styles.desc}>Swimming Pool</div>
- 
+            <div className={styles.detailHover}>
+              Embark on a journey of relaxation and rejuvenation with our
+              resort's unparalleled amenities. From refreshing swimming pools
+              and rejuvenating spa treatments to exciting recreational
+              activities and exquisite dining options, every aspect of your stay
+              is designed to elevate your experience and create cherished
+              memories amidst the beauty of nature.
+            </div>
           </div>
         </div>
         <div className={styles.amCard}>
@@ -75,7 +89,13 @@ const Products = () => {
           <div className={styles.cardItem}>
             <Image src={ic4} width={100} className={styles.icImg} alt="img" />
             <div className={styles.desc}>Clubhouse</div>
-
+            <div className={styles.detailHover}>
+              Discover the heart of luxury and leisure at our resort's
+              clubhouse. Offering a sanctuary of sophistication, it's a haven
+              for relaxation and socializing. Immerse yourself in premium
+              amenities, from fitness facilities to exclusive lounges, and
+              elevate your stay to new heights of indulgence and enjoyment.
+            </div>
           </div>
         </div>
       </div>
