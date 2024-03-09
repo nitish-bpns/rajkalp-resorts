c2af46e73b491bb376c4817ce18436050093fd56
Nitish kumar <nitish.bpns@gmail.com>
2024-03-09 19:19:11 +0530

    dashboard ui

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,73 +1,121 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import Loader from "@/components/loader/Loader";
 import axios from "axios";
+import Image from "next/image";
+import logo from "./../../../public/assets/rajkalp/logo2.png";
+import Footer from "@/components/footer/Footer";
 
 function Layout({ children }) {
-
   const [verified, setVerified] = useState(false);
 
   const router = useRouter();
 
+  const path = usePathname();
+
   const handleLogout = () => {
     try {
-      axios.get('/api/auth/logout');
-      router.push('/admin/login');
-      alert("You were logged out!")
+      axios.get("/api/auth/logout");
+      router.push("/admin/login");
+      alert("You were logged out!");
     } catch (err) {
-      console.log("Some error occured!")
-      alert("Unable to logout, try after a minute.")
+      console.log("Some error occured!");
+      alert("Unable to logout, try after a minute.");
     }
-  }
+  };
 
   const handleRefresh = () => {
     window.location.reload();
-  }
+  };
 
   useEffect(() => {
     const verifyToken = () => {
       setVerified(false);
       try {
-        axios.get('/api/auth/verify-token');
+        axios.get("/api/auth/verify-token");
         setVerified(true);
       } catch (error) {
         setVerified(false);
-        router.push('/admin/login');
+        router.push("/admin/login");
       }
-    }
+    };
 
     verifyToken();
-
   }, []);
 
   if (!verified) {
-    return (
-      <Loader />
-    )
+    return <Loader />;
   }
 
   return (
     <>
       <div className={styles.nav}>
-        <div className={styles.col1}>
-          <Link href="/admin/dashboard/form">
-            <button className={styles.formBtn}>Form Data</button>
-          </Link>
-          <Link href="/admin/dashboard/newsletter">
-            <button className={styles.newsBtn}>Newsletter</button>
-          </Link>
-          
-        </div>
-
-        <div className={styles.col2}>
-          <button onClick={handleRefresh} className={styles.refreshBtn}>Refresh</button>
-          <button onClick={handleLogout} className={styles.signoutBtn}>Signout</button>
-        </div>
+        <Image
+          src={logo}
+          alt="img"
+          className={styles.logo}
+          width={600}
+          height="auto"
+        />
+      </div>
+      <div className={styles.ribbon}>
+        <Link href="/dashboard" className={styles.ribs}>
+          <button
+            className={
+              path === "/dashboard" ? styles.butActive : styles.ribbonBtn
+            }
+          >
+            Welcome Letter
+          </button>
+        </Link>
+        <Link href="/dashboard/profile" className={styles.ribs}>
+          <button
+            className={
+              path === "/dashboard/profile"
+                ? styles.butActive
+                : styles.ribbonBtn
+            }
+          >
+            Profile
+          </button>
+        </Link>
+        <Link href="/dashboard/tree" className={styles.ribs}>
+          <button
+            className={
+              path === "/dashboard/tree" ? styles.butActive : styles.ribbonBtn
+            }
+          >
+            View Tree
+          </button>
+        </Link>
+        <Link href="/dashboard/commission" className={styles.ribs}>
+          <button
+            className={
+              path === "/dashboard/commission"
+                ? styles.butActive
+                : styles.ribbonBtn
+            }
+          >
+            Commission
+          </button>
+        </Link>
+        <Link href="/dashboard/referral" className={styles.ribs}>
+          <button
+            className={
+              path === "/dashboard/referral"
+                ? styles.butActive
+                : styles.ribbonBtn
+            }
+          >
+            Get Your Referral ID
+          </button>
+        </Link>
       </div>
       {children}
+      <Footer />
     </>
   );
 }
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import styles from "./page.module.css";
 
 const Dashboard = () => {
   const [verified, setVerified] = useState(false);
@@ -26,16 +27,34 @@ const Dashboard = () => {
   }
 
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        height: "80vh",
-        width: "100vw",
-        justifyContent: "center",
-      }}
-    >
-      <h1>Welcome to Admin Portal!</h1>
+    <div className={styles.welcomeDiv}>
+      <h1>Welcome Aboard!</h1>
+      <br />
+      <br />
+      <h3 className={styles.welcomePara}>
+        Dear User, <br />
+        <br />
+        Welcome to Rajkalp Resorts!
+        <br /> <br />
+        Congratulations on joining our exclusive community! As a member, you'll
+        enjoy luxurious perks like a 5-day stay at Rajkalp Resorts in Tadoba and
+        12 lifetime weekend getaways.
+        <br />
+        <br />
+        Plus, with our Incentive Plan, you can earn rewards just by introducing
+        others to our platform. It's the perfect way to turn your dreams of
+        luxury living into reality.
+        <br />
+        <br />
+        Let's embark on this exciting journey together!
+        <br />
+        <br />
+        Best Regards,
+        <br />
+        Rajkalp Resorts & Precious Services
+        <br />
+        <br />
+      </h3>
     </div>
   );
 };
