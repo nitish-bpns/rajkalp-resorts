82a605fdf8908067262df7d16222b693a967cac5
Nitish kumar <nitish.bpns@gmail.com>
2024-03-21 16:48:24 +0530

    update policy

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -21,60 +21,66 @@ const Footer = () => {
           width={1000}
           placeholder="blur"
         />
+        <div className={styles.policy}>
+          <a
+            href="https://docs.google.com/document/d/15xkaiDcH77NWj4NOvCAoFhgetrzZo3BKoZcLNxDefsE/edit?usp=sharing"
+            target="_blank"
+            className={styles.hlink}
+          >
+            Terms and Conditions
+          </a>
+        </div>
+        <div className={styles.policy}>
+          <a
+            href="https://docs.google.com/document/d/1AcY-zM3lUDA_d2Y2NCEUe9dSMF-MalnZ6tUzMxt8pqc/edit?usp=sharing"
+            target="_blank"
+            className={styles.hlink}
+          >
+            Refund Policy
+          </a>
+        </div>
       </div>
       <div className={styles.col2}>
-      <div className={styles.footHead2}>Contact Us</div>
-      <div className={styles.cont}>
-        <div className={styles.conta}>
-        <Image
-          src={call}
-          alt="img"
-          className={styles.footic}
-          width={500}
-        />
+        <div className={styles.footHead2}>Contact Us</div>
+        <div className={styles.cont}>
+          <div className={styles.conta}>
+            <Image src={call} alt="img" className={styles.footic} width={500} />
+          </div>
+          <div className={styles.contb}>+91 9922420555/7709860575</div>
         </div>
-        <div className={styles.contb}>
-          +91 9922420555/7709860575
-        </div>
-      </div>
 
-      <div className={styles.cont}>
-        <div className={styles.conta}>
-        <Image
-          src={gmail}
-          alt="img"
-          className={styles.footic}
-          width={500}
-        />
+        <div className={styles.cont}>
+          <div className={styles.conta}>
+            <Image
+              src={gmail}
+              alt="img"
+              className={styles.footic}
+              width={500}
+            />
+          </div>
+          <div className={styles.contb}>rajkalpresorts@gmail.com</div>
         </div>
-        <div className={styles.contb}>
-        rajkalpresorts@gmail.com
-        </div>
-      </div>
 
-      <div className={styles.cont}>
-        <div className={styles.conta}>
-        <Image
-          src={net}
-          alt="img"
-          className={styles.footic}
-          width={500}
-        />
+        <div className={styles.cont}>
+          <div className={styles.conta}>
+            <Image src={net} alt="img" className={styles.footic} width={500} />
+          </div>
+          <div className={styles.contb}>www.rajkalpresorts.com</div>
         </div>
-        <div className={styles.contb}>
-        www.rajkalpresorts.com
-        </div>
-      </div>
       </div>
       <div className={styles.col3}>
-      <div className={styles.footHead2}>Send Enquiry</div>
-      <form>
-        <input type="text" placeholder="Name" className={styles.name} />
-        <input type="text" placeholder="Contact No." className={styles.name} />
-        <input type="email" placeholder="Email Id" className={styles.name} />
-        <input type="text" placeholder="Message" className={styles.name} />
-        <button className={styles.sendBtn}>Send</button>
-      </form>
+        <div className={styles.footHead2}>Send Enquiry</div>
+        <form>
+          <input type="text" placeholder="Name" className={styles.name} />
+          <input
+            type="text"
+            placeholder="Contact No."
+            className={styles.name}
+          />
+          <input type="email" placeholder="Email Id" className={styles.name} />
+          <input type="text" placeholder="Message" className={styles.name} />
+          <button className={styles.sendBtn}>Send</button>
+        </form>
       </div>
     </div>
   );
